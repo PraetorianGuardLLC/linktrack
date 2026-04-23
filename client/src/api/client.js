807e98bfd88a8bb3bda90565df0b7397eafee196
Ghost <ghost@Praetorean.Russia>
2026-04-23 15:32:40 -0600

    hardcode railway api url

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL 
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : '/api',
+  baseURL: 'https://linktrack-server-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
